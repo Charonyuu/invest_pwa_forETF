5124d233c1c435e8273c2c84c8ca773c2a46cdf7
Charonyuu <bensona1219@gmail.com>
2022-11-13 02:46:08 +0800

    test error

--- src/hooks/useFetchData.js
+++ src/hooks/useFetchData.js
@@ -18,7 +18,7 @@ export default function useFetchData(){
             setLoading(false)
         }).catch((err)=>{
             console.log('error:' + err);
-            setError(err)
+            setError(JSON.stringify(err))
         })
     })
     useEffect(()=>{
