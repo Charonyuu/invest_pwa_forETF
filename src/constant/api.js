266539091f4aaa1131362b432230c58bb3295661
Charonyuu <bensona1219@gmail.com>
2022-11-13 01:48:38 +0800

    update api url

--- src/constant/api.js
+++ src/constant/api.js
@@ -1,4 +1,4 @@
-export const INVEST_ETF_WORTHS_BASE_URL = 'http://localhost:8000/investETF_worths';
+export const INVEST_ETF_WORTHS_BASE_URL = 'https://charonyuu.github.io/invest_pwa_forETF:8000/investETF_worths';
 
 export const STOCK_COMPANY_ARRAY=[
     '街口','台新','第一金','新光','中信','統一','FT','凱基',
